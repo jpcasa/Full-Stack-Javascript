96d58a9aa2ebf4d001f57fb46361c374640da020
Juan Pablo Casabianca <juanpablo@rabinovicionline.com>
2018-07-05 15:16:42 -0500

    Work in Progress

--- DevelopmentEnv/buildScripts/srcServer.js
+++ DevelopmentEnv/buildScripts/srcServer.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 // Define Port
 const port = 3000;
 
 // Creates Express App
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 // Handles the home URL
 app.get('/', (req, res) => {
